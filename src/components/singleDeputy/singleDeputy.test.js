e59d2a94eb274d9de99a60a0d0d186b820edc0fc
Romain Tourrel <romaintourrel@gmail.com>
2017-03-11 18:53:19 +0100

    add react router example with tests

---
+++ src/components/singleDeputy/singleDeputy.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SingleDeputy from './singleDeputy';
+
+it('renders SingleDeputy without params without crashing', () => {
+  const div = document.createElement('div');
+  expect(() => ReactDOM.render(<SingleDeputy />, div)).toThrow('SingleDeputy: please provide a match parameter.');
+});
+
+it('renders SingleDeputy without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<SingleDeputy match={{ params: { id: 'a' } }}/>, div);
+});
