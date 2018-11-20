d71f3f382fd7416656cd64c1bc92c0f29aa25e6f
PierreT <pierre.tusseau@gmail.com>
2018-11-21 00:31:44 +0100

    Tentative intégration composant dans liste de députés

--- src/components/listDeputy/listDeputy.js
+++ src/components/listDeputy/listDeputy.js
@@ -1,6 +1,8 @@
-import React, { Component } from "react";
-import "./listDeputy.css";
-import { getDeputiesInOffice } from "lbp-wrapper";
+import React, { Component } from "react"
+import { getDeputiesInOffice } from "lbp-wrapper"
+import OneDeputy from "./oneDeputy"
+import "./listDeputy.css"
+import "../home/home.css"
 
 class ListDeputy extends Component {
   constructor(...props) {
@@ -20,25 +22,29 @@ class ListDeputy extends Component {
         }
       })
     )
-    .catch(err => console.error("Error:", err));
+    .catch(err => console.error("Error:", err))
   }
   render() {
     if (!this.state.loaded) { // Pendant que je charge
       return null
     } else { // Si mes députés ont chargé
       console.log(this.state.deputes) // Récupéré mon objet dans la console
-      var that = this // Redéfinir le this pour dans une fonction de haute priorité
-      const deputeNames = this.state.deputes.result.map(function (deputeID) {
+      var that = this // Redéfinir le this pour le récupérer à l'intérieur du map
+      const listDeputies = this.state.deputes.result.map(function (deputeID) {
+        // return <OneDeputy
+        //   key={deputeID}
+        //   data={that.state.deputes.entities.depute[deputeID]}
+        // />
         return <a
           href={that.state.deputes.entities.depute[deputeID].url_an}
           id={"depute-" + deputeID} key={deputeID}
-          class="depute"
+          className="depute"
           target="_blank">
             {that.state.deputes.entities.depute[deputeID].nom}
           </a>
       })
       return (
-        deputeNames
+        listDeputies
       )
     }
   }
