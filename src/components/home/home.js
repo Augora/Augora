0b961cab44908f78725ac791a35d07b571a77456
PierreT <pierre.tusseau@gmail.com>
2018-11-21 00:31:44 +0100

    Tentative intégration composant dans liste de députés

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import { getDeputiesInOffice } from "lbp-wrapper";
+import "./home.css";
+
+const content = (
+  <div>
+    <h1>Home</h1>
+    <a href="/deputes">Liste des députés</a>
+  </div>
+)
 
 class Home extends Component {
   render() {
     getDeputiesInOffice()
       .then(d => console.log(d))
       .catch(err => console.error("Error:", err));
-    return <h1>Home</h1>;
+    return (content)
   }
 }
 
