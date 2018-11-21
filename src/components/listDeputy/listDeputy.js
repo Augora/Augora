b307905df1e47f4b22a1a34d9b4876e6d52ebb4b
PierreT <pierre.tusseau@gmail.com>
2018-11-21 19:50:50 +0100

    Clean folder + integration deputies list

--- src/components/listDeputy/listDeputy.js
+++ src/components/listDeputy/listDeputy.js
@@ -1,53 +1,16 @@
 import React, { Component } from "react"
-import { getDeputiesInOffice } from "lbp-wrapper"
-import OneDeputy from "./oneDeputy"
+import ListDeputies from "./listDeputies/listDeputies"
 import "./listDeputy.css"
 import "../home/home.css"
 
 class ListDeputy extends Component {
-  constructor(...props) {
-    super(...props)
-    this.state = {
-      loaded: false,
-      deputes: null,
-    }
-  }
-  componentDidMount() {
-    getDeputiesInOffice()
-    .then(
-      depute => this.setState(function (state) {
-        return {
-          loaded: true,
-          deputes: depute,
-        }
-      })
-    )
-    .catch(err => console.error("Error:", err))
-  }
   render() {
-    if (!this.state.loaded) { // Pendant que je charge
-      return null
-    } else { // Si mes députés ont chargé
-      console.log(this.state.deputes) // Récupéré mon objet dans la console
-      var that = this // Redéfinir le this pour le récupérer à l'intérieur du map
-      const listDeputies = this.state.deputes.result.map(function (deputeID) {
-        // return <OneDeputy
-        //   key={deputeID}
-        //   data={that.state.deputes.entities.depute[deputeID]}
-        // />
-        return <a
-          href={that.state.deputes.entities.depute[deputeID].url_an}
-          id={"depute-" + deputeID} key={deputeID}
-          className="depute"
-          target="_blank">
-            {that.state.deputes.entities.depute[deputeID].nom}
-          </a>
-      })
-      return (
-        listDeputies
-      )
-    }
+    return (
+      <section className="deputes__wrapper">
+        <ListDeputies />
+      </section>
+    )
   }
 }
 
-export default ListDeputy;
+export default ListDeputy
