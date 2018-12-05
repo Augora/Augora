416565253abec35a221b190b38c8e9cfdf1fd830
pierretusseau <pierre.tusseau@gmail.com>
2018-12-05 23:16:40 +0100

    Correction DeputyName structure

--- src/components/listDeputy/listDeputies/oneDeputy/deputyName/deputyName.js
+++ src/components/listDeputy/listDeputies/oneDeputy/deputyName/deputyName.js
@@ -3,13 +3,13 @@ import './deputyName.css'
 
 class DeputyName extends Component {
     render() {
-        return <div
+        return <h2
             className="depute__name"
             style={{ color: this.props.color }}>
                 {this.props.name} (id: {this.props.idAn})
                 <br/>
                 <span className="depute__age">{this.props.age}</span>
-        </div>
+        </h2>
     }
 }
 
