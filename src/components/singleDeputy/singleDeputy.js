a8eb6393c28586ddb683a9958052d75e8ae6e23e
kevinbacas <bacas.kevin@hotmail.fr>
2019-04-10 19:42:28 +0200

    Updated lbp-wrapper and internal code to handle changes

--- src/components/singleDeputy/singleDeputy.js
+++ src/components/singleDeputy/singleDeputy.js
@@ -1,43 +1,43 @@
-import React, { Component } from 'react';
-import { getDeputy } from 'lbp-wrapper';
+import React, { Component } from "react";
+import { getDeputy } from "lbp-wrapper";
 
 class SingleDeputy extends Component {
-    constructor(...props) {
-        super(...props);
+  constructor(...props) {
+    super(...props);
 
-        this.state = {
-            isLoading: true,
-            data: null,
-            error: null
-        };
-    }
+    this.state = {
+      isLoading: true,
+      data: null,
+      error: null
+    };
+  }
 
-    componentDidMount() {
-        const _this = this;
-        getDeputy(this.props.match.params.id).then(d =>
-            _this.setState(state =>
-                Object.assign({}, state, { isLoading: false, data: d })
-            )
-        );
-    }
+  componentDidMount() {
+    const _this = this;
+    getDeputy(this.props.match.params.id).then(d =>
+      _this.setState(state =>
+        Object.assign({}, state, { isLoading: false, data: d })
+      )
+    );
+  }
 
-    render() {
-        if (!this.props.match) {
-            throw new Error('SingleDeputy: please provide a match parameter.');
-        }
-        if (this.state.isLoading) {
-            return <h1>Loading...</h1>;
-        }
-        return (
-            <div>
-                <h1>SingleDeputy: {this.props.match.params.id}</h1>
-                <img
-                    src={this.state.data.entities.depute[1].imageDynamic(200)}
-                    alt=""
-                />
-            </div>
-        );
+  render() {
+    if (!this.props.match) {
+      throw new Error("SingleDeputy: please provide a match parameter.");
     }
+    if (this.state.isLoading) {
+      return <h1>Loading...</h1>;
+    }
+    return (
+      <div>
+        <h1>SingleDeputy: {this.props.match.params.id}</h1>
+        <img
+          src={this.state.data.imageDynamic(200)}
+          alt=""
+        />
+      </div>
+    );
+  }
 }
 
 export default SingleDeputy;
