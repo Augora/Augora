d2a1639bb4e987a08b39a8395234f8b396659f08
kevinbacas <bacas.kevin@hotmail.fr>
2019-04-13 14:18:01 +0200

    Add StyledComponents + few examples

--- src/components/listDeputy/listDeputies/oneDeputy/deputyName/deputyName.js
+++ src/components/listDeputy/listDeputies/oneDeputy/deputyName/deputyName.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
-import './deputyName.css';
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const DeputyNameHeader = styled.h2`
+  background: white;
+  padding: 10px;
+  margin: 0;
+  width: calc(90% - 2px);
+  text-transform: uppercase;
+  font-size: 15px;
+  font-weight: bold;
+  line-height: 1.5;
+`;
+
+const DeputyLink = styled.a`
+  color: ${props => props.color};
+`;
+
+const DeputyAge = styled.span`
+  color: ${props => props.color};
+`;
 
 class DeputyName extends Component {
-    render() {
-        return (
-            <a href={`/depute/${this.props.id}`}>
-                <h2
-                    className="depute__name"
-                    style={{ color: this.props.color }}
-                >
-                    {this.props.name} (id: {this.props.idAn})
-                    <br />
-                    <span className="depute__age">{this.props.age}</span>
-                </h2>
-            </a>
-        );
-    }
+  render() {
+    return (
+      <DeputyNameHeader>
+        <DeputyLink href={`/depute/${this.props.id}`} color={this.props.color}>
+          {this.props.name} (id: {this.props.idAn})
+        </DeputyLink>
+        <br />
+        <DeputyAge color={this.props.color}>{this.props.age}</DeputyAge>
+      </DeputyNameHeader>
+    );
+  }
 }
 
 export default DeputyName;
--- src/components/listDeputy/listDeputy.js
+++ src/components/listDeputy/listDeputy.js
@@ -1,22 +1,31 @@
-import React, { Component } from 'react';
-import ListDeputies from './listDeputies/listDeputies';
-import './listDeputy.css';
-import '../home/home.css';
+import React, { Component } from "react";
+import styled from "styled-components";
+
+import ListDeputies from "./listDeputies/listDeputies";
+
+const DeputesWrapper = styled.section`
+  margin-top: 120px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 10px;
+`;
 
 class ListDeputy extends Component {
-    render() {
-        return (
-            <div className="pageListing">
-                <header className="header">
-                    <h1>Liste des députés</h1>
-                    <a href="/">Retour à l'accueil</a>
-                </header>
-                <section className="deputes__wrapper">
-                    <ListDeputies />
-                </section>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="pageListing">
+        <header className="header">
+          <h1>Liste des députés</h1>
+          <a href="/">Retour à l'accueil</a>
+        </header>
+        <DeputesWrapper>
+          <ListDeputies />
+        </DeputesWrapper>
+      </div>
+    );
+  }
 }
 
 export default ListDeputy;
