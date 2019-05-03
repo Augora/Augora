3786506b960010a9800dd2d535fb317d099ca527
kevinbacas <bacas.kevin@hotmail.fr>
2019-05-03 17:28:10 +0200

    Fix case sensitive changes + react hooks demo

--- src/Deputy/Deputy.tsx
+++ src/Deputy/Deputy.tsx
@@ -1,51 +1,38 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { getDeputy } from "lbp-wrapper";
 
-// Define the state shape
-interface DeputyState {
-  isLoading: boolean;
-  data: IDeputy | null;
-}
-
-// Define the props shape
-interface DeputyProps {
+interface RouterProps<T> { 
   match: {
-    params: {
-      id: string;
-    };
+    params: T;
   };
 }
 
-class Deputy extends Component<DeputyProps, DeputyState> {
-  constructor(props: DeputyProps) {
-    super(props);
+interface RouterParams {
+  id: string;
+}
 
-    this.state = {
-      isLoading: true,
-      data: null
-    };
+function Deputy(props: RouterProps<RouterParams>): JSX.Element {
+  const [deputy, setDeputy] = useState<IDeputy | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getDeputy(props.match.params.id).then(d => {
+      setDeputy(d);
+      setIsLoading(false);
+    });
+  }, [props.match.params.id]);
+
+  if (isLoading || deputy === null) {
+    return <h1>Loading...</h1>;
   }
 
-  componentDidMount() {
-    const _this = this;
-    getDeputy(this.props.match.params.id).then(d =>
-      _this.setState(state =>
-        Object.assign({}, state, { isLoading: false, data: d })
-      )
-    );
-  }
-
-  render() {
-    if (this.state.isLoading || this.state.data === null) {
-      return <h1>Loading...</h1>;
-    }
-    return (
-      <div>
-        <h1>Deputy: {this.props.match.params.id}</h1>
-        <img src={this.state.data.imageDynamic(200)} alt="" />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Deputy: {props.match.params.id}</h1>
+      <img src={deputy.imageDynamic(200)} alt="" />
+    </div>
+  );
 }
 
 export default Deputy;
--- src/Home/Home.test.js
+++ src/Home/Home.test.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from './node_modules/react';
+import ReactDOM from './node_modules/react-dom';
 import Home from './Home';
 
 it('renders Home without crashing', () => {
