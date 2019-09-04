a3380d5af4d218de84d617197b692973365ed2dd
kevinbacas <bacas.kevin@hotmail.fr>
2019-09-04 09:58:09 +0200

    Move to gatsby, first iteration

---
+++ gatsby-config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  siteMetadata: {
+    title: `Augora`,
+    description: ``,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Augora`,
+        short_name: `Augora`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/Logos/Projet/lbp-logo.png`,
+      },
+    },
+    {
+      resolve: "gatsby-source-graphql-universal",
+      options: {
+        typeName: "DeputesEnMandat",
+        fieldName: "augora",
+        url: "https://api.augora.fr/Handler/graphql.go",
+      },
+    },
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-plugin-graphql-codegen`,
+      options: {
+        fileName: `types/graphql-types.ts`,
+        codegen: true,
+        codegenDelay: 250,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/templates/Deputy.tsx
@@ -0,0 +1,69 @@
+import React from "react"
+import Helmet from "react-helmet"
+import { graphql } from "gatsby"
+
+import GeneralInformation from "../components/Deputy/GeneralInformation/GeneralInformation"
+import {
+  getGender,
+  getGeneralInformation,
+  // getCoworkers
+} from "./Utils/Deputy.utils"
+// import Coworkers from "../Components/Deputy/Coworkers/Coworkers";
+import styled from "styled-components"
+
+import { SingleDeputyQuery } from "../../types/graphql-types"
+
+type SingleDeputyQueryProps = {
+  data: SingleDeputyQuery
+}
+const DeputyStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: 25vw;
+  grid-gap: 20px;
+  padding: 20px;
+  min-height: 100vh;
+`
+
+function Deputy({ data }: SingleDeputyQueryProps) {
+  const deputy = data.augora.Depute
+  return (
+    <DeputyStyles className="single-deputy">
+      <Helmet>
+        <meta name="robots" content="noindex,nofollow" />
+        <title>
+          {deputy.prenom} {deputy.nomDeFamille} - {getGender(deputy)}{" "}
+          {deputy.groupeSigle}
+        </title>
+      </Helmet>
+      <GeneralInformation {...getGeneralInformation(deputy, 500)} />
+    </DeputyStyles>
+  )
+}
+
+export default Deputy
+
+export const query = graphql`
+  query SingleDeputy($slug: DeputesEnMandat_string!) {
+    augora {
+      Depute(slug: $slug) {
+        groupeSigle
+        lieuNaissance
+        mandatDebut
+        nom
+        nomCirco
+        nomDeFamille
+        nombreMandats
+        numCirco
+        numDepartement
+        partiRattFinancier
+        placeEnHemicyle
+        prenom
+        profession
+        sexe
+        slug
+        twitter
+      }
+    }
+  }
+`
