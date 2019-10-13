bb6d0234d345bea3b233ee601740f61b1273a177
pierretusseau <pierre.tusseau@gmail.com>
2019-10-13 22:01:24 +0200

    Création d'un bloc qui peut recevoir une map mapbox

--- src/Components/Deputy/MapCirco/MapCirco.tsx
+++ src/Components/Deputy/MapCirco/MapCirco.tsx
@@ -1,33 +1,42 @@
-import React, { FunctionComponent, useState } from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
-import { NullLiteral } from "typescript";
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const Block = styled.div`
+  display: flex;
+  flex-direction: column;
+  grid-column-end: span 1;
+  background-color: rgba(0,0,0,0.15);
+  border: solid 2px rgba(0,0,0,0.25);
+  border-radius: 2px;
+  padding: 10px;
+`;
+const MapBlock = styled.div`
+  width: 100%;
+  height: 100%;
+`
 
 // use it to resize the box as you wish, maybe with default values ?
 interface CssValues {}
 
-export interface Geodata {
-  coworkers: String;
+export default class MapCirco extends Component {
+  componentDidMount() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia29iYXJ1IiwiYSI6ImNrMXBhdnV6YjBwcWkzbnJ5NDd5NXpja2sifQ.vvykENe0q1tLZ7G476OC2A';
+    var map = new mapboxgl.Map({
+      container: document.querySelector('.map'), // container id
+      style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+      center: [-74.50, 40], // starting position [lng, lat]
+      zoom: 9 // starting zoom
+    });
+  }
+
+  render = () => {
+    return (
+      <Block>
+          <MapBlock ref={this.mapRef} className="map">
+          </MapBlock>
+      </Block>
+    )
+  }
 }
-
-function MapCirco(props : Geodata) {
-  // Declare only one state, refresh all properties in once
-
-  const Block = styled.div`
-    display: flex;
-    flex-direction: column;
-    grid-column-end: span 1;
-    background-color: rgba(0,0,0,0.15);
-    border: solid 2px rgba(0,0,0,0.25);
-    border-radius: 2px;
-    padding: 10px;
-  `;
-  return (
-    <Block>
-        <div className="map">
-            Je suis une map
-        </div>
-    </Block>
-  );
-};
-
-export default MapCirco;
