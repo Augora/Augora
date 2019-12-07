8481294fa66c7c00f098ee1e8fd00b111a554e4b
pierretusseau <pierre.tusseau@gmail.com>
2019-12-07 14:31:37 +0100

    Sort les fonctions du composant MapCirco

--- src/Components/Deputy/MapCirco/MapCirco.jsx
+++ src/Components/Deputy/MapCirco/MapCirco.jsx
@@ -5,96 +5,99 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import GEOJsonCirco from './listCirco.json';
 import { retirerAccentsFR } from 'Utils/utils';
 
+const Block = styled.div`
+    display: flex;
+    flex-direction: column;
+    grid-column-end: span 1;
+    background-color: rgba(0,0,0,0.15);
+    border: solid 2px rgba(0,0,0,0.25);
+    border-radius: 2px;
+    padding: 10px;
+`;
+
+const MapBlock = styled.div`
+    width: 100%;
+    height: 100%;
+`
+
+const France = {
+    center: {lng: 1.88, lat: 46.60},
+    northWest: {lng: -6.864165, lat: 50.839888},
+    southEast: {lng: 13.089067, lat: 41.284012}
+}
+
+const getSelectedCircoBox = (map, selectedCirco, props) => {
+    // Récupérer le NW et SE du(des) polygone(s) de la Circonscription
+    let boxListOfLng = []
+    let boxListOfLat = []
+    
+    if (selectedCirco.geometry.type === 'Polygon') {
+    selectedCirco.geometry.coordinates[0].forEach(coords => {
+        boxListOfLng.push(coords[0])
+        boxListOfLat.push(coords[1])
+    })
+    } else {
+    selectedCirco.geometry.coordinates.forEach(polygon => {
+        polygon[0].forEach(coords => {
+        boxListOfLng.push(coords[0])
+        boxListOfLat.push(coords[1])
+        })
+    })
+    }
+    const selectedCircoBox = [
+    [Math.min(...boxListOfLng), Math.max(...boxListOfLat)],
+    [Math.max(...boxListOfLng), Math.min(...boxListOfLat)]
+    ]
+
+    drawSelectedCircoBox(map, selectedCirco, selectedCircoBox, props)
+}
+
+const drawSelectedCircoBox = (map, circo, box, props) => {
+    // Dessiner la circo
+    map.addLayer({
+    'id': props.nom.toLowerCase() + '-' + props.num,
+    'type': 'fill',
+    'source': {
+        'type': 'geojson',
+        'data': circo
+    },
+    'layout': {},
+    'paint': {
+        'fill-color': '#fff',
+        'fill-opacity': 0.5,
+        'fill-outline-color': '#f00'
+    }
+    })
+    if (box) {
+    setTimeout(() => {
+        map.fitBounds(box,  {
+        padding: 10,
+        maxZoom: 9
+        })
+    }, 1000)
+    }
+}
+
+const initMap = props => {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia29iYXJ1IiwiYSI6ImNrMXBhdnV6YjBwcWkzbnJ5NDd5NXpja2sifQ.vvykENe0q1tLZ7G476OC2A';
+    const map = new mapboxgl.Map({
+        container: document.querySelector('.map'), // container id
+        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+        center: France.center, // starting position [lng, lat]
+        zoom: 2, // starting zoom
+    });
+    map.on('style.load', () => {
+        // Récupérer la circonscription concernée
+        const selectedCirco = GEOJsonCirco.features.find((circo) => {
+            return circo.properties.nom_dpt.toLowerCase() === retirerAccentsFR(props.nom.toLowerCase()) && parseInt(circo.properties.num_circ) === props.num
+        })
+        getSelectedCircoBox(map, selectedCirco, props)
+    })
+}
+
 const MapCirco = (props) => {
-    const Block = styled.div`
-        display: flex;
-        flex-direction: column;
-        grid-column-end: span 1;
-        background-color: rgba(0,0,0,0.15);
-        border: solid 2px rgba(0,0,0,0.25);
-        border-radius: 2px;
-        padding: 10px;
-    `;
-    const MapBlock = styled.div`
-        width: 100%;
-        height: 100%;
-    `
-
     useEffect(() => {
-        const France = {
-            center: {lng: 1.88, lat: 46.60},
-            northWest: {lng: -6.864165, lat: 50.839888},
-            southEast: {lng: 13.089067, lat: 41.284012}
-        }
-        const getSelectedCircoBox = (map, selectedCirco) => {
-            // Récupérer le NW et SE du(des) polygone(s) de la Circonscription
-            let boxListOfLng = []
-            let boxListOfLat = []
-            
-            if (selectedCirco.geometry.type === 'Polygon') {
-            selectedCirco.geometry.coordinates[0].forEach(coords => {
-                boxListOfLng.push(coords[0])
-                boxListOfLat.push(coords[1])
-            })
-            } else {
-            selectedCirco.geometry.coordinates.forEach(polygon => {
-                polygon[0].forEach(coords => {
-                boxListOfLng.push(coords[0])
-                boxListOfLat.push(coords[1])
-                })
-            })
-            }
-            const selectedCircoBox = [
-            [Math.min(...boxListOfLng), Math.max(...boxListOfLat)],
-            [Math.max(...boxListOfLng), Math.min(...boxListOfLat)]
-            ]
-
-            drawSelectedCircoBox(map, selectedCirco, selectedCircoBox)
-        }
-        const drawSelectedCircoBox = (map, circo, box) => {
-            // Dessiner la circo
-            map.addLayer({
-            'id': props.nom.toLowerCase() + '-' + props.num,
-            'type': 'fill',
-            'source': {
-                'type': 'geojson',
-                'data': circo
-            },
-            'layout': {},
-            'paint': {
-                'fill-color': '#fff',
-                'fill-opacity': 0.5,
-                'fill-outline-color': '#f00'
-            }
-            })
-            if (box) {
-            setTimeout(() => {
-                map.fitBounds(box,  {
-                padding: 10,
-                maxZoom: 9
-                })
-            }, 1000)
-            }
-        }
-
-        const initMap = () => {
-            mapboxgl.accessToken = 'pk.eyJ1Ijoia29iYXJ1IiwiYSI6ImNrMXBhdnV6YjBwcWkzbnJ5NDd5NXpja2sifQ.vvykENe0q1tLZ7G476OC2A';
-            const map = new mapboxgl.Map({
-                container: document.querySelector('.map'), // container id
-                style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-                center: France.center, // starting position [lng, lat]
-                zoom: 2, // starting zoom
-            });
-            map.on('style.load', () => {
-                // Récupérer la circonscription concernée
-                const selectedCirco = GEOJsonCirco.features.find((circo) => {
-                    return circo.properties.nom_dpt.toLowerCase() === retirerAccentsFR(props.nom.toLowerCase()) && parseInt(circo.properties.num_circ) === props.num
-                })
-                getSelectedCircoBox(map, selectedCirco)
-            })
-        }
-
-        initMap()
+        initMap(props)
     }, [props])
 
     return(
