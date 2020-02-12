6c4cb9e2502efcb4a6e3047b3af40695a50c86c3
Pierre 'Kobaru' Tusseau <pierre.tusseau@gmail.com>
2020-02-12 21:26:26 +0100

    Reproduit le style tooltip sur le graphique en barre

--- src/Components/DeputiesList/BarChart/BarChart.js
+++ src/Components/DeputiesList/BarChart/BarChart.js
@@ -2,6 +2,25 @@ import React from "react"
 import { ResponsiveBar } from "@nivo/bar"
 import { getColorLuminosity, getTextColorContrast } from "Utils/utils"
 
+const generateTooltip = tooltip => {
+  return (
+    <div className="tooltip" style={{ whiteSpace: "pre" }}>
+      <span
+        style={{
+          display: "inline-block",
+          width: "12px",
+          height: "12px",
+          background: `${tooltip.data.color}`,
+          marginRight: "7px",
+        }}
+      ></span>
+      <span>
+        {tooltip.data.label} - {tooltip.data.value}
+      </span>
+    </div>
+  )
+}
+
 export default function BarChart(props) {
   return (
     <ResponsiveBar
@@ -30,6 +49,7 @@ export default function BarChart(props) {
           ? getTextColorContrast("light")
           : getTextColorContrast("dark")
       }
+      tooltip={tooltip => generateTooltip(tooltip)}
     />
   )
 }
