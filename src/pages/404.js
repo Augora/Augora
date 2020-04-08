844bbf843eb7459dcb01445f81864587077da1e4
Pierre "Kobaru" Tusseau <pierre.tusseau@gmail.com>
2020-04-08 16:00:50 +0200

    Commence le style de la page 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,15 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import "./404.scss"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="page__404">
+      <h1>404</h1>
+      <p>Not found</p>
+    </div>
   </Layout>
 )
 
