1561a8ccd09f2267a52b98c855467e1077d3b725
Pierre "Kobaru" Tusseau <pierre.tusseau@gmail.com>
2020-04-13 14:37:01 +0200

    Corrige les Styled-Components

--- src/components/deputy/_block/_Block.js
+++ src/components/deputy/_block/_Block.js
@@ -2,15 +2,17 @@ import React from "react"
 import Header from "./Header"
 import styled from "styled-components"
 
+const Block = styled.div`
+  border-color: ${(props) => props.color};
+`
+const BlockBackground = styled.div`
+  background-color: ${(props) => props.color};
+`
+
 export default function _Block(props) {
-  const Block = styled.div`
-    border-color: ${props.color};
-  `
-  const BlockBackground = styled.div`
-    background-color: ${props.color};
-  `
   return (
     <Block
+      color={props.colors}
       className={`deputy__block block__${props.type} deputy__block--${
         props.size ? props.size : "medium"
       }`}
@@ -18,6 +20,7 @@ export default function _Block(props) {
       <Header type={props.type} title={props.title} color={props.color} />
       {props.type === "general" ? (
         <BlockBackground
+          color={props.color}
           className={`block__background ${props.type}__background`}
         />
       ) : null}
