b1c48b92662aac93689ae0819cf276f0c79effc0
Pierre TUSSEAU <pierre.tusseau@gmail.com>
2020-07-28 01:03:31 +0200

    Begins about page (wip)

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const About = () => (
+  <Layout>
+    <SEO title="A propos de nous" />
+    <div className="page__about">
+      <h1>A propos d'Augora</h1>
+      <p>Nous sommes une association</p>
+    </div>
+  </Layout>
+)
+
+export default About
