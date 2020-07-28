5ce6ef29e097b1bfe937cdfccee055925a801b57
kevinbacas <bacas.kevin@hotmail.fr>
2020-07-28 12:11:35 +0200

    fix coworkers name

--- src/components/deputy/coworkers/coworker/Coworker.tsx
+++ src/components/deputy/coworkers/coworker/Coworker.tsx
@@ -6,13 +6,16 @@ import React from "react"
  */
 function Coworker(props) {
   const nameArray = props.coworker.split(" ")
+  const title = nameArray.shift()
+  const justName = nameArray.join(" ")
+
   return (
     <div className="coworkers__coworker">
       <p className="coworker__name">
         <span className="coworker__name-title" style={{ color: props.color }}>
-          {nameArray[0]}
+          {title}
         </span>
-        {nameArray[1]} {nameArray[2]}
+        {justName}
       </p>
     </div>
   )
