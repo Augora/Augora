3c6a6de5bb13d334b3d36dca312eb45af2f1553d
Narcasung <forkanerz@gmail.com>
2020-07-28 22:17:08 +0200

    Stylize age chart's tooltip

--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -7,18 +7,43 @@ export const Tooltip = (tooltip, total) => {
       style={{
         whiteSpace: "pre",
         backgroundColor: "white",
+        boxShadow: "0px 5px 5px rgba(0, 0, 0, 0.15)",
         padding: 10,
-        borderRadius: 10,
+        borderRadius: 5,
         fontFamily: "Open Sans",
+        minWidth: 200,
       }}
     >
-      <div className="tooltip__age">
-        <span>{tooltip.indexValue}</span> Ans
-      </div>
-      <div className="tooltip__groupe">
+      <div
+        className="tooltip__age"
+        style={{
+          fontSize: 24,
+          display: "flex",
+          marginBottom: 10,
+          justifyContent: "center",
+        }}
+      >
         <span
           style={{
-            display: "inline-block",
+            fontWeight: "bold",
+            paddingRight: 10,
+          }}
+        >
+          {tooltip.indexValue}
+        </span>
+        Ans
+      </div>
+      <div
+        className="tooltip__groupe"
+        style={{
+          marginBottom: 10,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <span
+          style={{
             width: "12px",
             height: "12px",
             background: `${tooltip.color}`,
@@ -27,9 +52,34 @@ export const Tooltip = (tooltip, total) => {
         ></span>
         <span>{tooltip.id}</span>
       </div>
-      <div className="tooltip__numbers">
-        <div className="tooltip__value">
-          <span>{tooltip.value}</span> Députés
+      <div
+        className="tooltip__numbers"
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          fontSize: 24,
+          fontWeight: "bold",
+        }}
+      >
+        <div
+          className="tooltip__value"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            fontSize: 13,
+            fontWeight: "normal",
+          }}
+        >
+          <span
+            style={{
+              fontSize: 24,
+              fontWeight: "bold",
+              paddingRight: 10,
+            }}
+          >
+            {tooltip.value}
+          </span>
+          Députés
         </div>
         <div className="tooltip__percentage">
           {Math.round(((tooltip.value * 100) / total) * 100) / 100}%
