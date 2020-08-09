40889eff2068e3799a57312f283beba2e8df93e7
Pierre TUSSEAU <pierre.tusseau@gmail.com>
2020-08-09 13:50:37 +0200

    CU-6ahzrd Tries to update og image for facebook

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,11 +24,14 @@ const Layout = ({ children }) => {
     <>
       <Helmet>
         <style>{`:root {\n${allColors.join("")}}`}</style>
-	<meta name="og:url" content="https://augora.fr" />
+        <meta name="og:url" content="https://augora.fr" />
         <meta name="og:type" content="website" />
         <meta name="og:title" content="Site web Augora" />
         <meta name="og:description" content="" />
         <meta name="og:image" content="/icons/icon-512x512.png" />
+        <meta name="og:image:url" content="/icons/icon-512x512.png" />
+        <meta name="og:image:secure_url" content="/icons/icon-512x512.png" />
+        <meta name="og:image:secure" content="/icons/icon-512x512.png" />
         <meta name="og:image:alt" content="Icône de l'association Augora" />
         <meta name="twitter:url" content="https://augora.fr" />
         <meta name="twitter:card" content="summary_large_image" />
