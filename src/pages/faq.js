134b3af613895a9b830ffb78f3b7d3c3d396b75b
goldenivan <maxrivolet@gmail.com>
2020-09-07 17:17:17 +0200

    CU-7yp81p Correction des fautes et ajout de parties

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -7,24 +7,43 @@ import Question from "components/faq/Question"
 // Content
 const contentAbout = [
   {
-    title: "Qu’est-ce qu’un groupe parlementaire ?",
-    description: (
-      <p>
-        Un groupe parlementaire n'est pas un parti politique à proprement
-        parler.
-      </p>
-    ),
-  },
-  {
-    title: "Qui sommes nous ?",
+    title: "Qui sommes-nous ?",
     description: (
       <p>
         Nous sommes une association à but non lucratif qui a pour vocation de
         mettre à disposition des outils d'informations statistiques sur les
-        députés via Augora.fr
+        députés via Augora.fr.
         <br />
-        Si vous souhaitez en savoir plus sur l'association, consultez le lien{" "}
-        <Link to="/about">A propos de nous</Link>
+      </p>
+    ),
+  },
+  {
+    title: "Qu’est-ce qu’un groupe parlementaire ?",
+    description: (
+      <p>
+        Un groupe parlementaire est un ensemble composé de minimum 15 députés.
+        Il permet de s'organiser au sein de l'Assemblée Nationale. Les députés
+        qui composent un groupe peuvent faire partie de différents mouvements
+        politiques.
+      </p>
+    ),
+  },
+  {
+    title: "Qu’est-ce qu’un député 'non-inscrit' ?",
+    description: (
+      <p>
+        Lorsqu'un député fait parti d'un groupe parlementaire de moins de 15
+        députés, il est assigné aux non-inscrits.
+      </p>
+    ),
+  },
+  {
+    title: "Comment est géré l'affichage des députés dans le listing ?",
+    description: (
+      <p>
+        Nous avons choisit un affichage aléatoire des députés pour éviter de
+        mettre certains groupes en avant, dans un soucis de transparence. Tous
+        les jours, l'ordre des députés affichés change.
       </p>
     ),
   },
@@ -32,19 +51,14 @@ const contentAbout = [
     title: "Quelles sont nos sources ?",
     description: (
       <p>
-        Nous utilisons des données publiques (open-data) venant de plusieurs
-        sources :<br />
-        <ul>
-          <li>
-            <Link
-              target="_blank"
-              to="https://www.regardscitoyens.org"
-              rel="noopener"
-            >
-              https://www.regardscitoyens.org
-            </Link>
-          </li>
-        </ul>
+        Nous utilisons des données publiques (open-data) venant de :{" "}
+        <Link
+          target="_blank"
+          to="https://www.regardscitoyens.org"
+          rel="noopener"
+        >
+          https://www.regardscitoyens.org
+        </Link>
       </p>
     ),
   },
@@ -53,23 +67,19 @@ const contentAbout = [
     description: (
       <p>
         Nous utilisons plusieurs technologies dont : Gatsby/React, FaunaDB,
-        Github, <br />
-        Nos méthodes de communication sont Twitter, Facebook, Instagram,
+        Github, Mapbox. <br />
+        Nous communiquons exclusivement via Twitter.
       </p>
     ),
   },
   {
-    title: "Quels données collectons-nous sur les utilisateurs du site ?",
+    title: "Quelles données collectons-nous sur les utilisateurs du site ?",
     description: (
       <p>
         Souhaitant établir une démarche de transparence avec nos utilisateurs,
         le site Augora ne souhaite collecter aucune données.
         <br />
-        En revanche les outils que nous utilisons comme FaunaDB, ..., ...
-        peuvent collecter certaines données, tel que ..., …<br />
-        Il est possible que des services que nous utilisions récupèrent des
-        données. Nous sommes en train de nous renseigner pour avoir plus de
-        détails.
+        Pour plus d'informations, se référer à la page des mentions légales.
       </p>
     ),
   },
@@ -78,8 +88,8 @@ const contentAbout = [
     description: (
       <p>
         Nous avons ouvert une page Tipeee afin de recueillir des dons, étant une
-        association à but non lucratif, nous ne retirons aucun avantages
-        personnel aux dons.
+        association à but non lucratif, nous ne retirons aucuns avantages
+        personnels aux dons.
       </p>
     ),
   },
@@ -89,7 +99,7 @@ const contentAbout = [
       <p>
         Grâce à vos dons, nous avons l'ambition de créer des outils toujours
         plus accessible et variés pour offrir à nos utilisateur une expérience
-        fluide et accessible sur différentes plateforme de diffusion
+        fluide et accessible sur différentes plateforme de diffusion.
         <br />
         Nous souhaitons développer un ensemble d’outils autour de la
         visualisation de données (data-visualisation) en association avec des
