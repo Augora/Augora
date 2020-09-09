64b25946328e899380293b2b8ee203cc8633302a
Narcasung <forkanerz@gmail.com>
2020-09-09 16:23:36 +0200

    Remove header bg and ghost close button

--- src/components/deputy/_block/Header.js
+++ src/components/deputy/_block/Header.js
@@ -8,16 +8,6 @@ export default function Header(props) {
   return (
     <div className={`${props.type}__header block__header`}>
       {props.type === "general" ? (
-        <div className="header__background">{/* Empty */}</div>
-      ) : (
-        <div
-          className="header__background"
-          style={{ backgroundColor: props.color }}
-        >
-          {/* Empty */}
-        </div>
-      )}
-      {props.type === "general" ? (
         <h2 className="header__title">{props.title}</h2>
       ) : (
         <h2
@@ -29,10 +19,6 @@ export default function Header(props) {
           {props.title}
         </h2>
       )}
-      <span className="block__close">
-        <span></span>
-        <span></span>
-      </span>
     </div>
   )
 }
