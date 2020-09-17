002bf16d41ada8468b01582545b03d73213c979e
Narcasung <narcasung@hotmail.fr>
2020-09-17 14:19:55 +0200

    Disable user interaction during transitions

--- src/components/deputy/map-district/MapDistrict.jsx
+++ src/components/deputy/map-district/MapDistrict.jsx
@@ -52,6 +52,11 @@ const getSelectedDistrictBox = (districtPolygon) => {
 
 export default function MapDistrict(props) {
   const [viewport, setViewport] = useState({})
+  const [interactionSettings, setInteractionSettings] = useState({
+    dragPan: false,
+    touchZoom: false,
+    scrollZoom: false,
+  })
   const [userInteracted, setUserInteracted] = useState(false)
   const [pinClicked, setPinClicked] = useState(false)
 
@@ -99,6 +104,26 @@ export default function MapDistrict(props) {
     flyToBounds(districtBox)
   }
 
+  //interaction handler
+  const handleInteraction = (state) => {
+    if (!state.inTransition) {
+      if (!interactionSettings.dragPan)
+        setInteractionSettings({
+          dragPan: true,
+          touchZoom: true,
+          scrollZoom: true,
+        })
+      if (!userInteracted && (state.isPanning || state.isZooming))
+        setUserInteracted(true)
+    } else if (interactionSettings.dragPan)
+      setInteractionSettings({
+        dragPan: false,
+        touchZoom: false,
+        scrollZoom: false,
+      })
+    if (pinClicked) setPinClicked(false)
+  }
+
   return (
     <Block
       title="Circonscription"
@@ -115,13 +140,13 @@ export default function MapDistrict(props) {
           mapboxApiAccessToken="pk.eyJ1Ijoia29iYXJ1IiwiYSI6ImNrMXBhdnV6YjBwcWkzbnJ5NDd5NXpja2sifQ.vvykENe0q1tLZ7G476OC2A"
           mapStyle="mapbox://styles/mapbox/streets-v11"
           {...viewport}
+          {...interactionSettings}
           width="100%"
           height="100%"
           minZoom={2}
           dragRotate={false}
           doubleClickZoom={false}
           touchRotate={false}
-          mapOptions={{}}
           //appelé Au chargement de la page
           onLoad={() => {
             setViewport({
@@ -132,20 +157,9 @@ export default function MapDistrict(props) {
             flyToBounds(districtBox)
           }}
           //appelé quand le viewport change - nécéssaire pour que la map bouge
-          onViewportChange={(change) => {
-            setViewport(change)
-          }}
+          onViewportChange={(change) => setViewport(change)}
           //appelé quand une interaction est constatée
-          onInteractionStateChange={(state) => {
-            if (
-              !userInteracted &&
-              !state.inTransition &&
-              (state.isPanning || state.isZooming)
-            ) {
-              setUserInteracted(true)
-            }
-            setPinClicked(false)
-          }}
+          onInteractionStateChange={(state) => handleInteraction(state)}
         >
           <Source type="geojson" data={districtPolygon}>
             <Layer
