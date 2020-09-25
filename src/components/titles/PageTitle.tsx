5fb8bd1cbcf9cda02781df9233fa74bc8a752b15
Narcasung <narcasung@hotmail.fr>
2020-09-25 13:20:03 +0200

    Add hover animation on page titles

--- src/components/titles/PageTitle.tsx
+++ src/components/titles/PageTitle.tsx
@@ -1,10 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 import GradientBanner from "../graphics/GradientBanner"
 
 export default function PageTitle(props: { title: string }) {
+  const [hovered, setHovered] = useState(false)
+
   return (
     <>
-      <div className="page-title">
+      <div
+        className={`page-title ${hovered ? "page-title--hovered" : ""}`}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      >
         <h1 className="page-title__title">{props.title}</h1>
         <GradientBanner />
       </div>
