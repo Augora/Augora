da8036b24a7b3a53a7b69bc7d0dcf64a483927ea
goldenivan <maxrivolet@gmail.com>
2021-01-08 14:43:05 +0100

    Renommage des tips et prise en compte de la couleur

--- src/components/charts/Bar.tsx
+++ src/components/charts/Bar.tsx
@@ -26,8 +26,9 @@ export default function BarChart({ width, height, margin, events = false, data }
   const yMax = height - verticalMargin
 
   // accessors
-  const x = (d) => d.id
-  const y = (d) => d.value
+  const sigle = (d) => d.id
+  const nombreDeputes = (d) => d.value
+  const colorGroupe = (d) => d.color
 
   // scales, memoize for performance
   const xScale = useMemo(
@@ -35,7 +36,7 @@ export default function BarChart({ width, height, margin, events = false, data }
       scaleBand<string>({
         range: [0, xMax],
         round: true,
-        domain: data.map(x),
+        domain: data.map(sigle),
         padding: 0.4,
       }),
     [xMax]
@@ -45,7 +46,7 @@ export default function BarChart({ width, height, margin, events = false, data }
       scaleLinear<number>({
         range: [yMax, 0],
         round: true,
-        domain: [0, Math.max(...data.map(y))],
+        domain: [0, Math.max(...data.map(nombreDeputes))],
       }),
     [yMax]
   )
@@ -55,31 +56,40 @@ export default function BarChart({ width, height, margin, events = false, data }
       <rect width={width} height={height} fill="url(#teal)" rx={14} />
       <Group top={verticalMargin / 2}>
         {data.map((d) => {
-          const letter = x(d)
+          const sigleAccessor = sigle(d)
           const barWidth = xScale.bandwidth()
-          const barHeight = yMax - (yScale(y(d)) ?? 0)
-          const barX = xScale(letter)
+          const barHeight = yMax - (yScale(nombreDeputes(d)) ?? 0)
+          const barX = xScale(sigleAccessor)
           const barY = yMax - barHeight
           return (
-            <Group key={`bar-${letter}`} left={margin.left} top={margin.top}>
+            <Group key={`bar-${sigleAccessor}`} left={margin.left} top={margin.top}>
               {/* <Group key={`bar-${letter}`}> */}
               <Bar
-                key={`bar-${letter}`}
+                key={`bar-${sigleAccessor}`}
                 x={barX}
                 y={barY}
                 width={barWidth}
                 height={barHeight}
-                fill="rgba(23, 233, 217, .5)"
+                //fill="rgba(23, 233, 217, .5)"
+                fill={colorGroupe(d)}
                 onClick={() => {
                   if (events) alert(`clicked: ${JSON.stringify(Object.values(d))}`)
                 }}
               />
               {/* <AxisLeft scale={yScale.range([yMax, 0])} /> */}
               <text x={barX} y={barY} fill="black" fontSize={12} dx={"-.2em"} dy={"-.33em"} style={{ fontFamily: "arial" }}>
-                {y(d)}
+                {nombreDeputes(d)}
               </text>
-              <text x={xScale(x(d))} y={yMax} fill="black" fontSize={12} dx={".32em"} dy={"1em"} style={{ fontFamily: "arial" }}>
-                {letter}
+              <text
+                x={xScale(sigle(d))}
+                y={yMax}
+                fill="black"
+                fontSize={12}
+                dx={".32em"}
+                dy={"1em"}
+                style={{ fontFamily: "arial" }}
+              >
+                {sigleAccessor}
               </text>
             </Group>
           )
