7fe2acbbb63f122debdfc3ad6395578d309bee31
Narcasung <narcasung@hotmail.fr>
2021-01-16 16:39:37 +0100

    type presence component

--- src/components/deputy/presence/Presence.tsx
+++ src/components/deputy/presence/Presence.tsx
@@ -1,20 +1,30 @@
 import React from "react"
 import Block from "../_block/_Block"
 
-const Presence = (props) => (
-  /**
-   * Return deputy's presence and participation graph in a Block component
-   * @param {*} props
-   */
-  <Block
-    title="Présence et participation"
-    type="presence"
-    color={props.color}
-    size={props.size}
-    wip={props.wip ? props.wip : false}
-  >
-    Je suis un graph
-  </Block>
-)
+interface IPresence {
+  color: string
+  size?: string
+  wip?: boolean
+}
+
+/**
+ * Return deputy's presence and participation graph in a Block component
+ * @param {*} props
+ */
+const Presence = (props: IPresence) => {
+  console.log(props)
+
+  return (
+    <Block
+      title="Présence et participation"
+      type="presence"
+      color={props.color}
+      size={props.size}
+      wip={props.wip ? props.wip : false}
+    >
+      Je suis un graph
+    </Block>
+  )
+}
 
 export default Presence
