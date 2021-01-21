ed3b81e0af1f277a2e54f496b672754a6a6400dc
goldenivan <maxrivolet@gmail.com>
2021-01-21 19:08:43 +0100

    Init des tooltips de la pyramide

--- src/components/charts/BarStack.tsx
+++ src/components/charts/BarStack.tsx
@@ -4,6 +4,8 @@ import { GridRows } from "@visx/grid"
 import { AxisLeft, AxisBottom } from "@visx/axis"
 import { scaleBand, scaleLinear, scaleOrdinal } from "@visx/scale"
 import { Group } from "@visx/group"
+import { useTooltip } from "@visx/tooltip"
+import ChartTooltip from "components/charts/ChartTooltip"
 
 interface BarStackProps {
   width: number
@@ -15,7 +17,16 @@ interface BarStackProps {
   averageAge: number
 }
 
+type TooltipData = {
+  key: string
+  bar: number
+  color: string
+}
+
+let tooltipTimeout: number
+
 export default function BarStackChart({ width, height, events = false, data, dataAge, maxAge, averageAge }: BarStackProps) {
+  const { tooltipOpen, tooltipLeft, tooltipTop, tooltipData, hideTooltip, showTooltip } = useTooltip<TooltipData>()
   // bounds
   const verticalMargin = 120
   const xMax = width
@@ -50,52 +61,79 @@ export default function BarStackChart({ width, height, events = false, data, dat
     else return ""
   }
 
+  const handleMouseLeave = () => {
+    tooltipTimeout = window.setTimeout(() => {
+      hideTooltip()
+    }, 300)
+  }
+
+  const handleMouseMove = (event, data) => {
+    if (tooltipTimeout) clearTimeout(tooltipTimeout)
+    const top = event.clientY
+    const left = event.clientX
+    showTooltip({
+      tooltipData: {
+        key: data.key,
+        bar: age(data),
+        color: data.color,
+      },
+      tooltipTop: top,
+      tooltipLeft: left,
+    })
+  }
+
   return width < 10 ? null : (
-    <svg width={width} height={height}>
-      <rect width={width} height={height} fill="url(#teal)" rx={14} />
-      <Group top={verticalMargin / 2}>
-        <AxisLeft scale={yScale.range([yMax, 0])} numTicks={6} />
-        <GridRows scale={yScale.range([yMax, 0])} width={xMax} height={yMax} stroke="#e0e0e0" numTicks={6} />
-        <text x="-70" y="15" transform="rotate(-90)" className="description_y">
-          Nombre de députés
-        </text>
-        <text x={xMax + 10} y={yMax} className="description_x">
-          Âge
-        </text>
-      </Group>
-      <Group top={verticalMargin / 2}>
-        <BarStack<{ age: any; [x: string]: number }, string>
-          data={dataAge}
-          keys={sigleList}
-          x={age}
-          xScale={xScale}
-          yScale={yScale}
-          color={colorScale}
-        >
-          {(barStacks) =>
-            barStacks.map((barStack) =>
-              barStack.bars.map((bar) => (
-                <rect
-                  key={`bar-stack-${barStack.index}-${bar.index}`}
-                  x={bar.x}
-                  y={bar.y}
-                  height={bar.height}
-                  width={bar.width}
-                  fill={bar.color}
-                />
-              ))
-            )
-          }
-        </BarStack>
-      </Group>
-      <Group top={verticalMargin / 2}>
-        <AxisBottom scale={xScale.range([xMax, 0])} top={yMax} numTicks={dataAge.map(age).length} />
-      </Group>
-      <Group top={verticalMargin}>
-        <text x={xMax / 2 - 33} y={yMax} className="age_moyen">
-          Âge moyen : {averageAge} ans
-        </text>
-      </Group>
-    </svg>
+    <div style={{ position: "relative" }}>
+      <svg width={width} height={height}>
+        <rect width={width} height={height} fill="url(#teal)" rx={14} />
+        <Group top={verticalMargin / 2}>
+          <AxisLeft scale={yScale.range([yMax, 0])} numTicks={6} />
+          <GridRows scale={yScale.range([yMax, 0])} width={xMax} height={yMax} stroke="#e0e0e0" numTicks={6} />
+          <text x="-70" y="15" transform="rotate(-90)" className="description_y">
+            Nombre de députés
+          </text>
+          <text x={xMax + 10} y={yMax} className="description_x">
+            Âge
+          </text>
+        </Group>
+        <Group top={verticalMargin / 2}>
+          <BarStack<{ age: any; [x: string]: number }, string>
+            data={dataAge}
+            keys={sigleList}
+            x={age}
+            xScale={xScale}
+            yScale={yScale}
+            color={colorScale}
+          >
+            {(barStacks) =>
+              barStacks.map((barStack) =>
+                barStack.bars.map((bar) => (
+                  <rect
+                    key={`bar-stack-${barStack.index}-${bar.index}`}
+                    x={bar.x}
+                    y={bar.y}
+                    height={bar.height}
+                    width={bar.width}
+                    fill={bar.color}
+                    onMouseLeave={handleMouseLeave}
+                    onMouseMove={(event) => handleMouseMove(event, bar)}
+                    //onMouseMove={(event) => console.log(bar)}
+                  />
+                ))
+              )
+            }
+          </BarStack>
+        </Group>
+        <Group top={verticalMargin / 2}>
+          <AxisBottom scale={xScale.range([xMax, 0])} top={yMax} numTicks={dataAge.map(age).length} />
+        </Group>
+        <Group top={verticalMargin}>
+          <text x={xMax / 2 - 33} y={yMax} className="age_moyen">
+            Âge moyen : {averageAge} ans
+          </text>
+        </Group>
+      </svg>
+      {tooltipOpen && tooltipData && <ChartTooltip tooltipTop={tooltipTop} tooltipLeft={tooltipLeft} tooltipData={tooltipData} />}
+    </div>
   )
 }
