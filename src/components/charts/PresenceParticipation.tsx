aa34c2b08042b54f68b3e917305e63d908597baa
goldenivan <maxrivolet@gmail.com>
2021-01-31 12:08:37 +0100

    Affichage des vacances dans le chart de présence

--- src/components/charts/PresenceParticipation.tsx
+++ src/components/charts/PresenceParticipation.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import _ from "lodash"
 import { Bar } from "@visx/shape"
 import { Group } from "@visx/group"
 import { AxisLeft, AxisBottom } from "@visx/axis"
@@ -10,15 +11,9 @@ import dayjs from "dayjs"
 import "dayjs/locale/fr"
 dayjs.locale("fr")
 
-const getDates = () => {
-  const now = dayjs()
-  const minusOneYear = now.subtract(1, "year")
-  let scaleTime = []
-  for (var i = 0; i < 13; i++) {
-    scaleTime.push(minusOneYear.add(i, "month").format("MMMM YYYY"))
-  }
+const getDates = (date: string) => {
   return {
-    scaleTime: scaleTime,
+    dateDay: dayjs(date).format("MMM YYYY"),
   }
 }
 
@@ -35,14 +30,12 @@ export default function PresenceParticipation({ width, height, data }) {
     domain: [0, getNbActivitesMax(data)],
   })
 
+  const orderedWeeks = _.orderBy(data, "NumeroDeSemaine")
   const dateScale = scaleBand({
-    domain: getDates().scaleTime,
+    domain: orderedWeeks.map((d) => getDates(d.DateDeFin.split("T")[0]).dateDay),
     range: [xMax, 0],
     padding: 0.15,
   })
-
-  const parseDate = timeParse("%Y-%m-%d")
-
   return width < 10 ? null : (
     <div className="presence">
       <svg width={width} height={height}>
@@ -56,18 +49,40 @@ export default function PresenceParticipation({ width, height, data }) {
           />
           <GridRows className="chart__rows" scale={activiteScale} width={xMax} height={yMax} numTicks={6} strokeWidth={2} />
         </Group>
-        {/* <Group top={marginTop / 2} left={marginLeft / 2}>
-          Data
-        </Group> */}
+        <Group top={marginTop / 2} left={marginLeft / 2}>
+          {orderedWeeks.map((d, index) => {
+            // filter sur Vacances
+            const barWidth = dateScale.bandwidth()
+            const barHeight = yMax
+            const barX = dateScale(getDates(d.DateDeFin.split("T")[0]).dateDay)
+            const barY = yMax - barHeight
+
+            if (d.Vacances !== 0) {
+              return (
+                <Group key={`bar-${d.DateDeFin}-${index}`}>
+                  <Bar
+                    x={barX}
+                    y={barY}
+                    rx="3" //border radius
+                    ry="3"
+                    width={barWidth}
+                    height={barHeight}
+                    fill={"grey"}
+                  />
+                </Group>
+              )
+            }
+          })}
+        </Group>
         <Group top={marginTop / 2} left={marginLeft / 2}>
           <AxisBottom
             axisClassName="chart__axislabel axislabel__bottom"
             tickClassName="chart__axistick"
             scale={dateScale.range([0, xMax])}
+            numTicks={11}
             top={yMax}
             hideAxisLine={true}
             tickLength={6}
-            rangePadding={2}
           />
         </Group>
       </svg>
