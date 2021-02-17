d6ef922a5b6e65e2bbbec49d2a5f45624b07edc4
goldenivan <maxrivolet@gmail.com>
2021-02-17 10:46:02 +0100

    Implémentation de la légende

--- src/components/charts/PresenceParticipation.tsx
+++ src/components/charts/PresenceParticipation.tsx
@@ -6,6 +6,10 @@ import { getNbActivitesMax } from "components/deputies-list/deputies-list-utils"
 import dayjs from "dayjs"
 import "dayjs/locale/fr"
 import { XYChart, AnimatedAxis, AnimatedBarSeries, AnimatedGrid, AnimatedLineSeries } from "@visx/xychart"
+import { Glyph as CustomGlyph, GlyphSquare } from "@visx/glyph"
+import { Legend, LegendItem, LegendLabel } from "@visx/legend"
+import { scaleOrdinal } from "@visx/scale"
+
 dayjs.locale("fr")
 
 const getDates = (date: string) => {
@@ -27,6 +31,21 @@ export default function PresenceParticipation({ width, height, data, color }) {
   const orderedWeeks = orderBy(data, "DateDeDebut")
   const animationTrajectoire = "center"
 
+  const shapeScale = scaleOrdinal<string, React.FC | React.ReactNode>({
+    domain: ["Présences", "Participations", "Questions orales", "Médiane des députés", "Vacances"],
+    range: [
+      <CustomGlyph left={50 / 6} top={50 / 6}>
+        <line x1="0" y1="0" x2="12" y2="0" stroke={color} strokeWidth={4} />
+      </CustomGlyph>,
+      <CustomGlyph left={50 / 6} top={50 / 6}>
+        <line x1="0" y1="0" x2="12" y2="0" stroke={color} strokeWidth={4} opacity={0.5} />
+      </CustomGlyph>,
+      <GlyphSquare key="Questions orales" size={120} top={50 / 6} left={50 / 6} fill={color} />,
+      <GlyphSquare key="Médiane des députés" size={120} top={50 / 6} left={50 / 6} fill="#B7B7B7" />,
+      <GlyphSquare key="Vacances" size={120} top={50 / 6} left={50 / 6} fill="#696969" />,
+    ],
+  })
+
   return width < 10 ? null : (
     <div className="presence">
       <svg width={width} height={height}>
@@ -44,7 +63,7 @@ export default function PresenceParticipation({ width, height, data, color }) {
                 data={orderedWeeks}
                 xAccessor={(d) => getDates(d.DateDeFin.split("T")[0]).dateFin ?? 0}
                 yAccessor={(d) => (d.Vacances ? maxActivite : 0)}
-                colorAccessor={() => "#B7B7B7"}
+                colorAccessor={() => "#696969"}
               />
             </>
             <>
@@ -104,6 +123,30 @@ export default function PresenceParticipation({ width, height, data, color }) {
           </XYChart>
         </Group>
       </svg>
+      <Legend scale={shapeScale}>
+        {(labels) => (
+          <div style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
+            {labels.map((label, i) => {
+              const shape = shapeScale(label.datum)
+              const isValidElement = React.isValidElement(shape)
+              return (
+                <LegendItem key={`legend-quantile-${i}`} flexDirection="row" margin="0 10px">
+                  <svg width={25} height={25}>
+                    {isValidElement
+                      ? React.cloneElement(shape as React.ReactElement)
+                      : React.createElement(shape as React.ComponentType<{ fill: string }>, {
+                          fill: color,
+                        })}
+                  </svg>
+                  <LegendLabel align="left" margin="0 0 12px 0">
+                    {label.text}
+                  </LegendLabel>
+                </LegendItem>
+              )
+            })}
+          </div>
+        )}
+      </Legend>
     </div>
   )
 }
