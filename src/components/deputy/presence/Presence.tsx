c1346be418c794241b7ce4031929884098165326
goldenivan <maxrivolet@gmail.com>
2021-02-18 16:42:31 +0100

    Ajout de l'icône derrière le bloc de présence

--- src/components/deputy/presence/Presence.tsx
+++ src/components/deputy/presence/Presence.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Block from "../_block/_Block"
 import PresenceParticipation from "src/components/charts/PresenceParticipation"
+import IconStat from "images/ui-kit/icon-stat.svg"
 
 /**
  * Return deputy's presence and participation graph in a Block component
@@ -11,6 +12,9 @@ const Presence = (props: Bloc.Presence) => {
   return (
     <Block title="Présence et participation" type="presence" color={props.color} size={props.size}>
       <PresenceParticipation width={1000} height={300} data={props.activite} mediane={props.mediane} color={props.color} />
+      <div className="icon-wrapper">
+        <IconStat />
+      </div>
     </Block>
   )
 }
