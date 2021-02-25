015bd278c2f1aad3e151bb76706c862e32d54cf3
goldenivan <maxrivolet@gmail.com>
2021-02-25 15:13:04 +0100

    Repositionnement du barstack

--- src/components/charts/BarStackChart.tsx
+++ src/components/charts/BarStackChart.tsx
@@ -1,6 +1,16 @@
 import React from "react"
 import AugoraTooltip from "components/tooltip/Tooltip"
-import { XYChart, AnimatedBarStack, AnimatedBarSeries, AnimatedGrid, AnimatedAxis, Tooltip } from "@visx/xychart"
+import { Group } from "@visx/group"
+import {
+  XYChart,
+  AnimatedBarStack,
+  AnimatedBarSeries,
+  AnimatedGrid,
+  AnimatedAxis,
+  Tooltip,
+  BarSeries,
+  BarStack,
+} from "@visx/xychart"
 
 interface BarStackProps extends Omit<Chart.BaseProps, "data"> {
   dataAge: Chart.AgeData[]
@@ -10,11 +20,13 @@ interface BarStackProps extends Omit<Chart.BaseProps, "data"> {
 
 export default function BarStackChart({ width, height, groups, dataAge, totalDeputes }: BarStackProps) {
   const maxAge = Math.max(...dataAge.map((d) => d.total))
-  const numTicks = 6
+  const numTicks = 4
   const listSigles = groups.map((g) => g.Sigle)
   const getGroupColor = (sigle: string): string => {
     return groups.find((group) => group.Sigle === sigle).Couleur
   }
+  const renderVertically = true
+
   // bounds
   const marginTop = 30
   const marginLeft = 20
@@ -24,62 +36,75 @@ export default function BarStackChart({ width, height, groups, dataAge, totalDep
   return (
     <div className="barstackchart chart">
       <svg width={width} height={height}>
-        <XYChart
-          margin={{ top: 0, right: 30, bottom: marginTop, left: 0 }}
-          width={width}
-          height={height}
-          yScale={{ type: "linear", range: [yMax, 0], round: true, domain: [0, maxAge] }}
-          xScale={{
-            type: "band",
-            range: [0, xMax],
-            domain: dataAge.map((d) => d.age as number),
-            padding: 0.15,
-          }}
-        >
-          <AnimatedGrid className="chart__rows" numTicks={numTicks} columns={false} />
-          <AnimatedAxis
-            axisClassName="chart__axislabel axislabel__verticalpyramide"
-            orientation="left"
-            hideAxisLine={true}
-            hideTicks={true}
-            numTicks={numTicks}
-          />
-          <AnimatedAxis
-            axisClassName="chart__axislabel axislabel__bottom"
-            orientation="bottom"
-            tickLength={6}
-            hideAxisLine={true}
-          />
-          <AnimatedBarStack>
-            {listSigles.map((sigle) => {
-              return (
-                <AnimatedBarSeries
-                  dataKey={sigle}
-                  data={dataAge}
-                  xAccessor={(data) => data.age}
-                  yAccessor={(data) => data.groups[sigle].length}
-                  colorAccessor={() => getGroupColor(sigle)}
-                />
-              )
-            })}
-          </AnimatedBarStack>
-          <Tooltip<Chart.AgeData>
-            className="charttooltip__container"
-            unstyled={true}
-            renderTooltip={({ tooltipData }) => {
-              const key = tooltipData.nearestDatum.key
-              return (
-                <AugoraTooltip
-                  title={key}
-                  nbDeputes={tooltipData.datumByKey[key].datum.total}
-                  totalDeputes={totalDeputes}
-                  color={getGroupColor(key)}
-                  age={tooltipData.datumByKey[key].datum.age}
-                />
-              )
-            }}
-          />
-        </XYChart>
+        <Group top={marginTop / 2} left={marginLeft / 2}>
+          <XYChart
+            margin={{ top: 0, right: 30, bottom: marginTop, left: 0 }}
+            width={width}
+            height={height}
+            xScale={
+              renderVertically
+                ? {
+                    type: "band",
+                    range: [0, xMax],
+                    padding: 0.15,
+                  }
+                : { type: "linear", range: [0, xMax], domain: [0, maxAge] }
+            }
+            yScale={
+              renderVertically
+                ? { type: "linear", range: [yMax, 0], domain: [0, maxAge] }
+                : {
+                    type: "band",
+                    range: [yMax, 0],
+                    padding: 0.15,
+                  }
+            }
+          >
+            <AnimatedGrid className="chart__rows" numTicks={numTicks} columns={false} />
+            <AnimatedAxis
+              axisClassName="chart__axislabel axislabel__verticalpyramide"
+              orientation="left"
+              hideAxisLine={true}
+              hideTicks={true}
+              numTicks={numTicks}
+            />
+            <AnimatedAxis
+              axisClassName="chart__axislabel axislabel__bottom"
+              orientation="bottom"
+              tickLength={6}
+              hideAxisLine={true}
+            />
+            <BarStack>
+              {listSigles.map((sigle) => {
+                return (
+                  <BarSeries
+                    dataKey={sigle}
+                    data={dataAge}
+                    xAccessor={(data) => (renderVertically ? data.age : data.groups[sigle].length)}
+                    yAccessor={(data) => (renderVertically ? data.groups[sigle].length : data.age)}
+                    colorAccessor={() => getGroupColor(sigle)}
+                  />
+                )
+              })}
+            </BarStack>
+            <Tooltip<Chart.AgeData>
+              className="charttooltip__container"
+              unstyled={true}
+              renderTooltip={({ tooltipData }) => {
+                const key = tooltipData.nearestDatum.key
+                return (
+                  <AugoraTooltip
+                    title={key}
+                    nbDeputes={tooltipData.datumByKey[key].datum.total}
+                    totalDeputes={totalDeputes}
+                    color={getGroupColor(key)}
+                    age={tooltipData.datumByKey[key].datum.age}
+                  />
+                )
+              }}
+            />
+          </XYChart>
+        </Group>
       </svg>
     </div>
   )
