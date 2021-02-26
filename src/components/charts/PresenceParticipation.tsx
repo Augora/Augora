24bda3fa882f8906860836d9adaf08565ea3198a
goldenivan <maxrivolet@gmail.com>
2021-02-26 20:51:42 +0100

    Intégration de la tooltip bloc présence

--- src/components/charts/PresenceParticipation.tsx
+++ src/components/charts/PresenceParticipation.tsx
@@ -139,6 +139,70 @@ export default function PresenceParticipation(props: IPresence) {
               animationTrajectory={animationTrajectoire}
               tickFormat={(date: string) => getDates(date.split("T")[0]).MonthData}
             />
+            <Tooltip<Chart.Activites>
+              className="charttooltip__container"
+              unstyled={true}
+              renderTooltip={({ tooltipData }) => {
+                const key = tooltipData.nearestDatum.index
+                const nearest = tooltipData.nearestDatum.datum
+                console.log("Présences", nearest.PresenceEnHemicycle + nearest.PresencesEnCommission)
+                console.log("Participations", nearest.ParticipationEnHemicycle + nearest.ParticipationsEnCommission)
+                console.log("Questions orales", nearest.Question)
+                return (
+                  <AugoraTooltip title={`Semaine du ${getDates(nearest.DateDeDebut).DayData}`}>
+                    <Legend scale={shapeScale}>
+                      {(labels) => (
+                        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
+                          {labels.map((label, i) => {
+                            const shape = shapeScale(label.datum)
+                            const isValidElement = React.isValidElement(shape)
+                            // Passer à 3 pour intégrer la mediane
+                            return i > 2 ? (
+                              ""
+                            ) : (
+                              <LegendItem key={`legend-quantile-${i}`} flexDirection="row">
+                                <div className="legend__col">
+                                  <svg width={25} height={25}>
+                                    {isValidElement
+                                      ? React.cloneElement(shape as React.ReactElement)
+                                      : React.createElement(shape as React.ComponentType<{ fill: string }>, {
+                                          fill: color,
+                                        })}
+                                  </svg>
+                                  <LegendLabel className="label">
+                                    {label.datum === "Questions orales"
+                                      ? "Questions"
+                                      : label.datum === "Mediane des députés"
+                                      ? "Mediane"
+                                      : label.text}
+                                  </LegendLabel>
+                                </div>
+                                <div className="legend__col">
+                                  <LegendLabel className="labelValue" align={"flex-end"}>
+                                    {label.datum === "Présences"
+                                      ? nearest.PresenceEnHemicycle + nearest.PresencesEnCommission != 0
+                                        ? nearest.PresenceEnHemicycle + nearest.PresencesEnCommission
+                                        : "0"
+                                      : null}
+                                    {label.datum === "Questions orales" ? (nearest.Question != 0 ? nearest.Question : "0") : null}
+                                    {label.datum === "Participations"
+                                      ? nearest.ParticipationEnHemicycle + nearest.ParticipationsEnCommission != 0
+                                        ? nearest.ParticipationEnHemicycle + nearest.ParticipationsEnCommission
+                                        : "0"
+                                      : null}
+                                    {label.datum === "Mediane" ? "0" : null}
+                                  </LegendLabel>
+                                </div>
+                              </LegendItem>
+                            )
+                          })}
+                        </div>
+                      )}
+                    </Legend>
+                  </AugoraTooltip>
+                )
+              }}
+            />
           </XYChart>
         </Group>
       </svg>
