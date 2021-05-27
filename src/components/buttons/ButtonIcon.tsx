78efca11529eac99f405464733231ae9a10c4c06
Narcasung <narcasung@hotmail.fr>
2021-05-27 18:53:18 +0200

    refactor buttonicon component

--- src/components/buttons/ButtonIcon.tsx
+++ src/components/buttons/ButtonIcon.tsx
@@ -1,16 +1,24 @@
 import React from "react"
 import Button from "./Button"
 
-export interface IButtonIcon {
-  className?: string
+/**
+ * Construct a type with the properties in both A and B that have the same type
+ *
+ * Loses optionality in the process
+ */
+type Common<A, B> = Pick<
+  A,
+  {
+    [K in keyof A & keyof B]: A[K] extends B[K] ? K : never
+  }[keyof A & keyof B]
+>
+
+type AnchorProps = React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>
+type BtnProps = React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+
+export interface IButtonIcon extends Partial<Omit<Common<AnchorProps, BtnProps>, "onClick">> {
   /** Soit une fonction, soit une URL */
   onClick: (<T>(args?: T) => void) | string
-  style?: React.CSSProperties
-  color?: string
-  children?: React.ReactNode
-  checked?: boolean
-  name?: string
-  title?: string
   target?: string
   deactivated?: boolean
 }
@@ -22,29 +30,21 @@ export interface IButtonIcon {
  * @param {Function} onClick - ... must contain onClick
  * @param {string} [color] - Color of the button
  * @param {React.ReactNode} [children] - Children content
- * @param {boolean} [checked] - Is the button checked
  * @param {ButtonType} [type] - Type of input
  */
 export default function ButtonIcon(props: IButtonIcon) {
-  const classNames = `${props.className} btn--icon ${props.deactivated ? "btn--deactivated" : ""}`
-  if (typeof props.onClick === "string") {
+  const { className, target, deactivated, children, onClick, ...restProps } = props
+  const classNames = `${className} btn--icon ${deactivated ? "btn--deactivated" : ""}`
+  if (typeof onClick === "string") {
     return (
-      <a href={props.onClick} className={`btn ${classNames}`} title={props.title} target={props.onClick ? props.target : ""}>
-        {props.children}
+      <a href={onClick} className={`btn ${classNames}`} title={props.title} target={onClick ? target : ""} {...restProps}>
+        {children}
       </a>
     )
   } else {
     return (
-      <Button
-        className={`${classNames} btn--${props.className.split(" ")[0]}`}
-        onClick={props.onClick}
-        title={props.title}
-        style={{
-          ...props.style,
-        }}
-        color={props.color}
-      >
-        {props.children}
+      <Button className={`${classNames} btn--${className.split(" ")[0]}`} onClick={onClick} {...restProps}>
+        {children}
       </Button>
     )
   }
