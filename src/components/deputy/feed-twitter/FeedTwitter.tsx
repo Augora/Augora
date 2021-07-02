465b866f92008305036bab34f0b58f8bb6227f68
goldenivan <maxrivolet@gmail.com>
2021-07-02 13:34:38 +0200

    Avoid twitter call not initialized

--- src/components/deputy/feed-twitter/FeedTwitter.tsx
+++ src/components/deputy/feed-twitter/FeedTwitter.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Block from "../_block/_Block"
 
 /**
@@ -6,17 +6,32 @@ import Block from "../_block/_Block"
  * @param {*} props
  */
 
+const checkTweets = (url) => {
+  useEffect(() => {
+    const script = document.createElement("script")
+    script.src = url
+    script.async = true
+    script.charset = "utf-8"
+
+    document.body.appendChild(script)
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [url])
+}
+
 const Feed = (props: Bloc.Base) => {
   return (
     <Block title="Feed Twitter" type="feed" color={props.color} size={props.size} wip={props.wip ? props.wip : false}>
+      {checkTweets("https://platform.twitter.com/widgets.js")}
       <a
         className="twitter-timeline"
         data-height="400"
         data-chrome="noheader nofooter transparent noscrollbar"
+        data-dnt="true"
         href={`https://twitter.com/${props.twitter}?ref_src=twsrc%5Etfw`}
         lang="fr"
       />
-      <script async src="//platform.twitter.com/widgets.js" charSet="utf-8" />
     </Block>
   )
 }
