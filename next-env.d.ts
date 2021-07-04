5b88e1b3281aee93df91929b394ffc2b6b026d5a
Pierre TUSSEAU <pierre.tusseau@gmail.com>
2021-07-04 15:36:54 +0200

    Fix Next11 new ts additional rules

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,17 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
-
-declare module "react-helmet"
-declare module "*.png"
-declare module "*.svg" {
-  import React = require("react")
-  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>
-  export default ReactComponent
-}
-declare module "*.jpg"
-declare module "*.geojson" {
-  const GeoJSONType: AugoraMap.FeatureCollection
-  export default GeoJSONType
-}
-
