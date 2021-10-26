5588194f0894211c6d3d9b8696bc87b0909610e1
Narcasung <narcasung@hotmail.fr>
2021-10-26 17:01:24 +0200

    use cdn instead of css import for fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,7 +23,10 @@ export default withRouter(function MyApp({ Component, pageProps, router }: AppPr
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href={`https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap`} rel="stylesheet" />
+        <link
+          href={`https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap`}
+          rel="stylesheet"
+        />
       </Head>
       <Layout location={router} title={pageProps.title}>
         <Component {...pageProps} />
