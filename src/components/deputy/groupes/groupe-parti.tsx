8186dacff440092c5ada7cd1413ceaedc7a7b4ab
goldenivan <maxrivolet@gmail.com>
2022-01-09 15:50:30 +0100

    Add information buttons

--- src/components/deputy/groupes/groupe-parti.tsx
+++ src/components/deputy/groupes/groupe-parti.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import Block from "../_block/_Block"
 import Image from "next/image"
+import IconInfo from "images/ui-kit/icon-info.svg"
+import ButtonIcon from "src/components/buttons/ButtonIcon"
 
 /**
  * Return deputy's contact info in a Block component
@@ -9,20 +11,39 @@ import Image from "next/image"
 const GroupeEtParti = (props: Bloc.GroupeEtParti) => {
   return (
     <Block title="Groupes et partis" type="groupes" color={props.color} size={props.size} wip={props.wip}>
-      <div className="groupes__title" style={{ color: props.color.HSL.Full }}>
-        Groupe Parlementaire
+      <div className="groupes__container">
+        <div className="groupes__title" style={{ color: props.color.HSL.Full }}>
+          Groupe Parlementaire
+        </div>
+        <ButtonIcon
+          onClick={"/faq#quest-ce-quun-groupe-parlementaire"}
+          className="groupes__button"
+          title={"Qu'est-ce qu'un groupe parlementaire ?"}
+          target=""
+        >
+          <IconInfo className={"icon-info"} style={{ fill: props.color.HSL.Full }} />
+        </ButtonIcon>
       </div>
+
       <div className="groupes__role" style={{ color: props.color.HSL.Full }}>
         {/* ({props.responsabiliteGroupe}) */}
       </div>
       <div className="groupes__nom" title={props.groupe}>
         <props.photoGroupe />
       </div>
-
-      <div className="rattachement__title" style={{ color: props.color.HSL.Full }}>
-        Rattachement financier
+      <div className="groupes__container">
+        <div className="rattachement__title" style={{ color: props.color.HSL.Full }}>
+          Rattachement financier
+        </div>
+        <ButtonIcon
+          onClick={"/faq#quest-ce-quun-rattachement-financier"}
+          className="groupes__button"
+          title={"Qu'est-ce qu'un rattachement financier ?"}
+          target=""
+        >
+          <IconInfo className={"icon-info"} style={{ fill: props.color.HSL.Full }} />
+        </ButtonIcon>
       </div>
-
       {props.photoRattachement != null ? (
         typeof props.photoRattachement === "function" ? (
           <div className="rattachement__image rattachement__svg" title={props.rattachement}>
