bab0c1fc649dff88a760c4087343cf5fd8422629
KevinBacas <bacas.kevin@hotmail.fr>
2022-01-18 21:34:39 +0100

    Add static props for other pages

--- src/pages/statistiques.tsx
+++ src/pages/statistiques.tsx
@@ -8,11 +8,12 @@ import Frame from "components/frames/Frame"
 import { ParentSize } from "@visx/responsive"
 import { getNbDeputiesGroup } from "components/deputies-list/deputies-list-utils"
 import useDeputiesFilters from "hooks/deputies-filters/useDeputiesFilters"
-import { getDeputes } from "src/lib/deputes/Wrapper"
+import { getDeputes, getGroupes } from "../lib/deputes/Wrapper"
 import PyramideBar from "src/components/charts/PyramideBar/PyramideBar"
 import PyramideBarStack from "src/components/charts/PyramideBar/PyramideBarStack"
 import { getAgeData, rangifyAgeData } from "components/charts/chart-utils"
 import IconSwitch from "images/ui-kit/icon-chartswitch.svg"
+import shuffle from "lodash/shuffle"
 
 type Groups = {
   id: string
@@ -162,11 +163,12 @@ export default function StatsPage() {
 }
 
 export async function getStaticProps() {
-  const deputes = await getDeputes()
+  const [deputes, groupes] = await Promise.all([getDeputes(), getGroupes()])
 
   return {
     props: {
-      deputes,
+      deputes: shuffle(deputes),
+      groupes,
       title: "Statistiques",
       PageType: PageType.Statistiques,
     },
