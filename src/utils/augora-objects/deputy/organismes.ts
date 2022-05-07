9c0d38a8060657c22cb0cfdd56b62c7b21b63f71
goldenivan <maxrivolet@gmail.com>
2022-05-07 10:52:32 +0200

    Set the permanent commission in first position on organisme list

--- src/utils/augora-objects/deputy/organismes.ts
+++ src/utils/augora-objects/deputy/organismes.ts
@@ -3,19 +3,26 @@
  * @param deputy
  */
 export function getOrganismes(deputy: Deputy.Deputy) {
-  const organismes = deputy.Depute_OrganismeParlementaire.map((o) => {
+  const organismesSorted = deputy.Depute_OrganismeParlementaire.sort(function (o, b) {
+    return o.OrganismeParlementaire.EstPermanent === b.OrganismeParlementaire.EstPermanent
+      ? 0
+      : o.OrganismeParlementaire.EstPermanent
+      ? -1
+      : 1
+  })
+
+  const organismes = organismesSorted.map((o) => {
     const organisme = {
       OrganismeNom: o.OrganismeParlementaire.Nom,
       Fonction: o.Fonction,
-      Permanente: o.OrganismeParlementaire.EstPermanent
+      Permanente: o.OrganismeParlementaire.EstPermanent,
     }
-
     return organisme
   })
 
   const props = {
     organismes: organismes,
-    nombre: organismes.length
+    nombre: organismes.length,
   }
 
   return props
