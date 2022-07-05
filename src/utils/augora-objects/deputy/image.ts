34e21509b04c8635d13ff9143d4fcf667396fbf0
Narcasung <narcasung@hotmail.fr>
2022-07-05 19:37:55 +0200

    add new group logos

--- src/utils/augora-objects/deputy/image.ts
+++ src/utils/augora-objects/deputy/image.ts
@@ -1,15 +1,19 @@
-import UDI from "images/logos/groupes-parlementaires/udi.svg"
 import GDR from "images/logos/groupes-parlementaires/gdr.svg"
 import LFI from "images/logos/groupes-parlementaires/lfi.svg"
+import ECO from "images/logos/groupes-parlementaires/eco.svg"
 import LR from "images/logos/groupes-parlementaires/lr.svg"
-import LREM from "images/logos/groupes-parlementaires/lrem.svg"
 import LT from "images/logos/groupes-parlementaires/lt.svg"
 import MODEM from "images/logos/groupes-parlementaires/modem.svg"
+import REN from "images/logos/groupes-parlementaires/ren.svg"
+import HOR from "images/logos/groupes-parlementaires/hor.svg"
 import NI from "images/logos/groupes-parlementaires/ni.svg"
 import PS from "images/logos/groupes-parlementaires/ps.svg"
-import AE from "images/logos/groupes-parlementaires/ae.svg"
-import RN from "images/logos/partis-politiques/rn.svg"
+import RN from "images/logos/groupes-parlementaires/rn.svg"
+import RN_PARTI from "images/logos/partis-politiques/rn.svg"
+import LFI_PARTI from "images/logos/partis-politiques/lfi.svg"
+import LR_PARTI from "images/logos/partis-politiques/lr.svg"
 import RPS from "images/logos/partis-politiques/rps.png"
+import LREM from "images/logos/partis-politiques/lrem.svg"
 import MD from "images/logos/partis-politiques/md.svg"
 import UDI_PARTI from "images/logos/partis-politiques/udi.png"
 import PCF from "images/logos/partis-politiques/pcf.svg"
@@ -22,7 +26,6 @@ import TAVINI from "images/logos/partis-politiques/tavini.png"
 import CE from "images/logos/partis-politiques/ce.png"
 import PS_PARTI from "images/logos/partis-politiques/ps.svg"
 import EELV from "images/logos/partis-politiques/eelv.svg"
-
 
 /**
  * Match the politic group's photo import with his acronym
@@ -44,28 +47,28 @@ export function getDeputyPhotoURL(slug: string, height: number) {
  */
 export function getPoliticGroupPhotoImport(politicGroupAcronym: string) {
   switch (politicGroupAcronym) {
-    case "UDI":
-      return UDI
+    case "ECO":
+      return ECO
     case "GDR":
       return GDR
-    case "LT":
-      return LT
     case "LFI":
       return LFI
-    case "LR":
-      return LR
-    case "LREM":
-      return LREM
-    case "MODEM":
-      return MODEM
-    case "NI":
-      return NI
     case "SOC":
       return PS
-    case "UAI":
-      return UDI
-    case "AE":
-      return AE
+    case "LT":
+      return LT
+    case "REN":
+      return REN
+    case "MODEM":
+      return MODEM
+    case "HOR":
+      return HOR
+    case "LR":
+      return LR
+    case "RN":
+      return RN
+    case "NI":
+      return NI
     default:
       return NI
   }
@@ -76,8 +79,8 @@ export function getPoliticPartyPhotoImport(party: string) {
     case "Parti radical de gauche":
       return PRG
     case "La France Insoumise":
-      return LFI
-    case "Tapura Huiraatira": 
+      return LFI_PARTI
+    case "Tapura Huiraatira":
       return TAPURA
     case "Cap sur l'avenir":
       return CAP
@@ -102,11 +105,11 @@ export function getPoliticPartyPhotoImport(party: string) {
     case "Union des démocrates, radicaux et libéraux":
       return UDI_PARTI
     case "Les Républicains":
-      return LR
+      return LR_PARTI
     case "Debout la France":
       return DLF
     case "Rassemblement national":
-      return RN
+      return RN_PARTI
     default:
       return null
   }
