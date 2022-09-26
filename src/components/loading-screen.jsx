dd414c667bcd9e84e0d7bba8b4a08ef9c28f66f0
Pierre Tusseau <pierre.tusseau@gmail.com>
2022-09-26 10:55:25 +0200

    wip - page transition
    Il y a actuellement un bug dans la transition de page, ce build
    n'est pas utilisable pour une release stable, je le laisse en
    l'état pour ne pas perdre le code déjà écrit, mais il y a des
    chances qu'on reparte sur des bases fraîches dans le futur

---
+++ src/components/loading-screen.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function LoadingScreen() {
+  return <div id="loading-screen">{/* Silence is golden */}</div>
+}
+
+export default LoadingScreen
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,69 @@ import { AppProps } from "next/app"
 import sortBy from "lodash/sortBy"
 
 import Layout from "components/layout"
+import LoadingScreen from "components/loading-screen"
+import { motion, AnimatePresence } from "framer-motion"
+import { gsap } from 'gsap'
 import { hydrateStoreWithInitialLists } from "stores/deputesStore"
 
 // Styles
 import "../styles/app.scss"
 
 export default withRouter(function MyApp({ Component, pageProps, router }: AppProps) {
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    let timer;
+    const aniStart = async (url) => {
+      console.log(`Loading: ${url} : loading = `, loading)
+      timer = setTimeout(() => {
+        if (!loading) {
+          setLoading(true);
+          const tl = gsap.timeline();
+          tl.to('#loading-screen', {
+            scaleX: 1,
+            duration: 0.2,
+            ease: 'Expo.easeInOut',
+            onComplete: () => {
+              document.querySelector('#loading-screen').classList.add('loading')
+            }
+          });
+        }
+      }, 300);
+    };
+    const aniEnd = () => {
+      console.log(`Loading completed : loading = `, loading)
+      if (timer) {
+        clearTimeout(timer);
+      }
+      const tl = gsap.timeline();
+      if (loading) {
+        tl.to('#loading-screen', {
+          scaleX: 0,
+          duration: 0.8,
+          ease: 'Expo.easeInOut',
+          onComplete: () => {
+            document.querySelector('#loading-screen').classList.remove('loading')
+          }
+        });
+        setLoading(false);
+      }
+
+      tl.set('#loading-screen', { scaleX: 0 });
+      clearTimeout(timer);
+    };
+
+    router.events.on('routeChangeStart', aniStart)
+    router.events.on('routeChangeComplete', aniEnd)
+    router.events.on('routeChangeError', aniEnd)
+
+    return () => {
+      router.events.off('routeChangeStart', aniStart)
+      router.events.off('routeChangeComplete', aniEnd)
+      router.events.off('routeChangeError', aniEnd)
+    }
+  }, [router])
+  
   if (pageProps.deputes) {
     const orderedDeputes = pageProps.deputes
     const orderedGroupes = sortBy(pageProps.groupes, "Ordre")
@@ -30,6 +87,21 @@ export default withRouter(function MyApp({ Component, pageProps, router }: AppPr
       <Layout location={router} title={pageProps.title}>
         <Component {...pageProps} />
       </Layout>
+      <LoadingScreen/>
+      {/* <AnimatePresence exitBeforeEnter> */}
+        {/* <motion.div 
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ 
+            duration: 2000,
+            // delay: 0.5,
+            // ease: [0, 0.71, 0.2, 1.01]
+          }}
+        >
+          {loading ? <LoadingScreen/> : null}
+        </motion.div> */}
+      {/* </AnimatePresence> */}
     </>
   )
 })
