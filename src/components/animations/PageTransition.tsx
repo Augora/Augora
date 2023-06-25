2ca02281f8ec0763a4ed3ab53b2757f717480f44
Narcasung <narcasung@hotmail.fr>
2023-06-25 19:03:22 +0200

    remove sliding panel from page transition

--- src/components/animations/PageTransition.tsx
+++ src/components/animations/PageTransition.tsx
@@ -6,33 +6,30 @@ import LoadingScreen from "components/frames/LoadingScreen"
 const PageTransition = ({ children }: { children: React.ReactNode }) => {
   const { pathname } = useRouter()
 
-  const [slideVisible, setSlideVisible] = useState(false)
-
   return (
     <div className="page-transition">
       <AnimatePresence mode="wait">
         <motion.div
           key={pathname}
+          initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          initial={false}
-          exit={{ opacity: 0.9 }}
-          transition={{ duration: 0.2 }}
-          onAnimationStart={() => setSlideVisible(true)}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.4, ease: "easeInOut" }}
         >
           {children}
         </motion.div>
       </AnimatePresence>
-      <AnimatePresence>
+      {/* <AnimatePresence>
         {slideVisible && (
           <LoadingScreen
-            initial={{ scaleX: 0, transformOrigin: "left" }}
-            animate={{ scaleX: 1 }}
-            exit={{ scaleX: 0, transformOrigin: "right", transition: { ease: "easeInOut", duration: 0.8 } }}
-            transition={{ ease: "easeInOut", duration: 0.2 }}
+            initial={{ x: "-100%" }}
+            animate={{ x: "0%" }}
+            exit={{ x: "100%", transition: { ease: "easeIn", duration: 0.3 } }}
+            transition={{ ease: "easeOut", duration: 0.3 }}
             onAnimationComplete={() => setSlideVisible(false)}
           />
         )}
-      </AnimatePresence>
+      </AnimatePresence> */}
     </div>
   )
 }
