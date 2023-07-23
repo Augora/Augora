542c1462c9841ee7926addb6f553b7a2eed6d711
Narcasung <narcasung@hotmail.fr>
2023-07-23 20:34:39 +0200

    fine tune background image parallax

--- src/components/home/Parallax.tsx
+++ src/components/home/Parallax.tsx
@@ -8,14 +8,14 @@ function Parallax({ img, intro = false, gradient = false }: { img: StaticImageDa
   const ref = useRef<HTMLImageElement>(null)
 
   const { scrollYProgress } = useScroll({ target: ref, offset: ["start end", "end start"] })
-  const yRange = useTransform(scrollYProgress, [!intro ? 0 : 0.5, 1], !intro ? [100, -250] : [0, -350]) // intro uses 0.5-1 range instead of 0-1 because `scrollYProgress` starts at 0.5 on the top of the page
+  const yRange = useTransform(scrollYProgress, [!intro ? 0 : 0.5, 1], !intro ? [50, -250] : [0, -300]) // intro uses 0.5-1 range instead of 0-1 because `scrollYProgress` starts at 0.5 on the top of the page
   const y = useSpring(yRange, { damping: 90 })
 
   return (
     <div className="background background--parallax" ref={ref}>
       <MotionNextImg
         className={`background__img ${gradient ? "background__img--transparent" : ""}`}
-        style={{ y, scale: 0.8 }}
+        style={{ y, scale: 0.7 }}
         src={img}
         alt="Photographie"
         placeholder="blur"
